4ea06f15a5c68f408d47f02cdb77a2abbe5836d2
Amelia Keibler <akeibler@sonatype.com>
2023-12-21 11:13:27 -0500

    Initial repository template

---
+++ README.md
@@ -0,0 +1,18 @@
+# Cypher Character Creator
+
+A series of tools for generating characters compatible with the Cypher System
+TTRPG system.
+
+## License
+
+This code is licensed under the [Be Gay, Do Crime](./LICENSE) license.
+
+### Disclaimer
+
+This product is an independent production and is not affiliated with Monte Cook
+Games, LLC. It is published under the Cypher System Open License, found at
+http://csol.montecookgames.com.
+
+CYPHER SYSTEM and its logo are trademarks of Monte Cook Games, LLC in the U.S.A.
+and other countries. All Monte Cook Games characters and character names, and
+the distinctive likenesses thereof, are trademarks of Monte Cook Games, LLC.
---
+++ flake.nix
@@ -0,0 +1,96 @@
+{
+  description = "A character creation tool that is compatible with the Cypher System";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, crane, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-analyzer" "rust-src" ];
+        };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = {
+          inherit src;
+
+          # uncomment if the project is a workspace
+          pname = "cypher_character_creator";
+          version = "0.1.0";
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        cypher_character_creator = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+      in
+      rec {
+        checks = {
+          inherit cypher_character_creator;
+
+          clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+          });
+
+          doc = craneLib.cargoDoc (commonArgs // {
+            inherit cargoArtifacts;
+          });
+
+          fmt = craneLib.cargoFmt (commonArgs // {
+            inherit src;
+          });
+        };
+
+        packages.cypher_character_creator = cypher_character_creator;
+        packages.default = packages.cypher_character_creator;
+
+        # uncomment if there is a binary to be run
+        # apps.cypher_character_creator = flake-utils.lib.mkApp {
+        #   drv = packages.cypher_character_creator;
+        #   name = "cypher_character_creator";
+        # };
+        # apps.default = apps.cypher_character_creator;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks.${system};
+
+          packages = with pkgs; [
+            rustToolchain
+            cargo-edit
+            cargo-msrv
+            cargo-outdated
+
+            # GitHub tooling
+            gh
+
+            # Nix tooling
+            nixpkgs-fmt
+          ];
+        };
+      });
+}
