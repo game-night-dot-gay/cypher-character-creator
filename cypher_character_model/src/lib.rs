371be4e86e12a9da5a887afa16b8e2d43860e14a
Amelia Keibler <akeibler@sonatype.com>
2023-12-21 17:33:33 -0500

    Create initial character model based on the sentence concept

--- cypher_character_model/src/lib.rs
+++ cypher_character_model/src/lib.rs
@@ -1,14 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// Character is the entry-point to the data model
+#[derive(Debug)]
+pub struct Character {
+    /// The name of the character
+    pub name: String,
+    /// The pronouns of the character
+    pub pronouns: String,
+    /// The Cypher System sentence that describes the character
+    pub sentence: Sentence,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Sentence is the high-level description of the character
+#[derive(Debug)]
+pub struct Sentence {
+    pub descriptor: String,
+    pub character_type: String,
+    pub flavor: Option<String>,
+    pub focus: String,
 }
