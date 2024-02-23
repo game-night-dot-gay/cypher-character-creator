a505620748d64353058c5788b65d2df3ed0ab7bf
Allison Sierra <85902135+allisonsierra@users.noreply.github.com>
2024-02-23 16:26:46 -0500

    Can serve a template

---
+++ cypher_character_frontend/src/main.rs
@@ -0,0 +1,66 @@
+use anyhow::Context;
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "cypher_character_frontend=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+        info!("initializing router...");
+
+        
+        let router = Router::new().route("/", get(hello));
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.context("error creating listener")?;
+
+        info!("router initialized, now listening on port 8000");
+        axum::serve(listener, router)
+            .await
+            .context("error while starting server")?;
+    
+        Ok(())
+}
+
+async fn hello() -> impl IntoResponse {
+    let template = HelloTemplate {};
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate;
+
+/// A wrapper type that we'll use to encapsulate HTML parsed by askama into valid HTML for axum to serve.
+struct HtmlTemplate<T>(T);
+
+/// Allows us to convert Askama HTML templates into valid HTML for axum to serve in the response.
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        // Attempt to render the template with askama
+        match self.0.render() {
+            // If we're able to successfully parse and aggregate the template, serve it
+            Ok(html) => Html(html).into_response(),
+            // If we're not, return an error or some bit of fallback HTML
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
+}
